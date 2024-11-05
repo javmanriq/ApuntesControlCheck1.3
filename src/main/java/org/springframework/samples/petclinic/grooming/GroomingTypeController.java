719b8776adc2a4e0e20dfab350d5836b17880aba
Javier Manrique Riquelme <Javier.Manrique688@gmail.com>
2024-11-05 13:31:07 +0100

    Test9 (POST)

--- src/main/java/org/springframework/samples/petclinic/grooming/GroomingTypeController.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/GroomingTypeController.java
@@ -2,14 +2,21 @@ package org.springframework.samples.petclinic.grooming;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.exceptions.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/groomingtypes")
@@ -30,6 +37,16 @@ public class GroomingTypeController {
             throw new ResourceNotFoundException("No existe un tipo con dicha id" + id);
         }
         return type;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<GroomingType> create(@RequestBody @Valid GroomingType gt) {
+        GroomingType newGT = new GroomingType();
+        GroomingType savedGT;
+        BeanUtils.copyProperties(gt, newGT, "id");
+        savedGT = this.gs.save(newGT);
+        return new ResponseEntity<>(savedGT, HttpStatus.CREATED);
     }
     
 }
