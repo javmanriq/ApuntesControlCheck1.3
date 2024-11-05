e5fd14fac89d736ff0e31441ca3b9ecef5cce273
Javier Manrique Riquelme <Javier.Manrique688@gmail.com>
2024-11-05 13:30:04 +0100

    Test8 (GET)

--- src/main/java/org/springframework/samples/petclinic/grooming/GroomingTypeController.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/GroomingTypeController.java
@@ -1,5 +1,35 @@
 package org.springframework.samples.petclinic.grooming;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.exceptions.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+@RestController
+@RequestMapping("/api/v1/groomingtypes")
+@SecurityRequirement(name = "bearerAuth")
 public class GroomingTypeController {
+    @Autowired
+    GroomingTypeService gs;
+
+    @GetMapping
+    public List<GroomingType> findAll() {
+        return gs.getAllGroomingTypes();
+    }
+
+    @GetMapping("/{id}")
+    public GroomingType findGroomingTypeById(@PathVariable("id") Integer id) {
+        GroomingType type = gs.getGroomingTypeById(id);
+        if (type == null) {
+            throw new ResourceNotFoundException("No existe un tipo con dicha id" + id);
+        }
+        return type;
+    }
     
 }
